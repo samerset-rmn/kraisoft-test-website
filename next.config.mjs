fb40f5e0be8227955b580d8907a043e7b0465d3e
Roman Kosov <samerset14@gmail.com>
2024-06-29 08:41:17 +0700

    Preparation for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  /**
+   * NOTE: GitHub Pages does not support server-side code. Therefore we need to set the build type to a static site.
+   * output, basePath, unoptimized images
+   */
+  output: 'export',
+  basePath: '/kraisoft-test-website',
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
