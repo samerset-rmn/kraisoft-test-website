522746499cdc34bdbae633dfd8047a0f7c2fe4e8
Roman Kosov <samerset14@gmail.com>
2024-06-30 08:22:51 +0700

    Add items spawn only on an empty space

--- src/features/game-field/game-field.tsx
+++ src/features/game-field/game-field.tsx
@@ -1,10 +1,23 @@
 import { IGameFieldProps } from './types';
+import { GAME_FIELD_ID } from './constants';
 
 import styles from './styles.module.scss';
 
 export const GameField: React.FC<React.PropsWithChildren<IGameFieldProps>> = ({ children, onClick }) => {
   return (
-    <div className={styles.container} onClick={(e) => onClick({ top: e.clientY, left: e.clientX })}>
+    <div
+      id={GAME_FIELD_ID}
+      className={styles.container}
+      onClick={(e) => {
+        /**
+         * NOTE Spawn new items only when clicked on an empty space.
+         * But if later we need to react to clicks on any item, this solution should be overwritten.
+         * For current requirements it's enough.
+         */
+        if ((e.target as HTMLDivElement).id === GAME_FIELD_ID) {
+          onClick({ top: e.clientY, left: e.clientX });
+        }
+      }}>
       {children}
     </div>
   );
