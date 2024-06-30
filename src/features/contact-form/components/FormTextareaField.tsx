780c40ff5deac696ff3f157a034be1d5917e4361
Roman Kosov <samerset14@gmail.com>
2024-06-30 15:24:58 +0700

    Fix styles

--- src/features/contact-form/components/FormTextareaField.tsx
+++ src/features/contact-form/components/FormTextareaField.tsx
@@ -7,7 +7,7 @@ import styles from '../styles/FormTextareaField.module.scss';
 export const FormTextareaField: React.FC<IFormFieldProps> = ({ field, placeholder, isError }) => {
   return (
     <div className={styles.textarea_wrapper} data-replicated-value={field.value}>
-      <textarea {...field} className={clsx(styles.textarea, isError && styles.invalid)} placeholder={placeholder} />
+      <textarea {...field} className={clsx(styles.textarea, isError && styles.invalid)} placeholder={placeholder} rows={4} />
     </div>
   );
 };
